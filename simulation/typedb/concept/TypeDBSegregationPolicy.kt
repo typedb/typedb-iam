87501aa3c2e8b226b0f1594976718790020187b1
James Whiteside <james.whiteside@vaticle.com>
2023-03-16 17:47:00 +0000

    Updated segregation policies to include policy names.

--- simulation/typedb/concept/TypeDBSegregationPolicy.kt
+++ simulation/typedb/concept/TypeDBSegregationPolicy.kt
@@ -1,3 +1,8 @@
 package com.vaticle.typedb.iam.simulation.typedb.concept
 
-data class TypeDBSegregationPolicy(val action1: TypeDBAction, val action2: TypeDBAction)
+import com.vaticle.typedb.client.api.concept.Concept
+import com.vaticle.typedb.iam.simulation.typedb.Util.stringValue
+
+data class TypeDBSegregationPolicy(val action1: TypeDBAction, val action2: TypeDBAction, val name: String) {
+    constructor(action1: TypeDBAction, action2: TypeDBAction, name: Concept): this(action1, action2, stringValue(name))
+}
